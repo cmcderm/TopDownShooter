d69761cc34e72705856148881ff88e7b0532ec47
loligator <loligator704@gmail.com>
2020-01-12 21:37:01 -0600

    Small changes to camera script

--- Assets/_Scripts/Utilities/CameraController.cs
+++ Assets/_Scripts/Utilities/CameraController.cs
@@ -1,22 +1,24 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Camera))]
 public class CameraController : MonoBehaviour {
 
-    Transform _player;
+    [SerializeField]
+    GameObject player;
 
-	// Use this for initialization
-	void Start () {
-        if (!_player) {
-            _player = Player.p.transform;
-        }
+    private Transform target;
+
+    // Use this for initialization
+    void Start () {
+        target = player?.transform ?? throw new System.Exception("Error, Camera not provided a player to watch.");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 newPos = new Vector3(_player.transform.position.x, _player.transform.position.y, transform.position.z);
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, transform.position.z);
         transform.position = newPos;
 	}
 }
